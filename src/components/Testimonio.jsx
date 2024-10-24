3ce2e1e015f5f673d8377ae15c6e432fe99453aa
MoisesAPC <moipc647@gmail.com>
2024-10-24 09:59:39 +0100

    Importada milibreria

--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -6,8 +6,10 @@ import { CardMedia } from '@mui/material';
 import { Typography } from '@mui/material';
 import { Avatar } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { MyButton } from 'milibreria';
+import { MyFavourite } from 'milibreria';
+import { MyFavouriteBorder } from 'milibreria';
+import { MoisesAntonioPC } from 'milibreria';
 
 const Testimonio = ({ nombre, pais, cargo, empresa, alt, testimonio, imagen }) => {
 
@@ -18,40 +20,71 @@ const Testimonio = ({ nombre, pais, cargo, empresa, alt, testimonio, imagen }) =
   };
 
   return (
-    <Card sx={{ maxWidth: 1000 }}>
-      <CardContent>
+    <>
+    {/* Añadimos un "div" para asegurarnos de que el componente "MoisesAntonioPC" esté debajo del Card por defecto de Testimonios */}
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
+      <Card sx={{ maxWidth: 1000 }}>
+        <CardContent>
+          <CardMedia
+            component="img"
+            image={imagen}
+            alt={alt}
+            title={alt}
+            sx={{
+              width: '100%',
+              height: 500,
+              marginBottom: '16px',
+              objectFit: 'cover'
+            }}
+          />
 
-      <CardMedia
-          component="img"
-          image={imagen}
-          alt={alt}
-          title={alt}
-          sx={{
-            width: '100%',
-            height: 500,
-            marginBottom: '16px',
-            objectFit: 'cover'
-          }}/>
+          <Typography variant="h6" component="div">
+            {nombre} en {pais}
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary">
+            {cargo} en {empresa}
+          </Typography>
+          <Typography variant="body2" color="text.secondary" sx={{ marginTop: '8px' }}>
+            "{testimonio}"
+          </Typography>
+          
+          <IconButton onClick={handlerMeGusta}>
+            {gustado ? <MyFavourite color="error" /> : <MyFavouriteBorder />}
+          </IconButton>
 
-        {/* <Avatar src={imagen} alt={alt} sx={{ width: 60, height: 60, marginBottom: '8px' }} /> */}
+          <MyButton 
+            text='Mi botón' 
+            txtcolor='black' 
+            bgcolor='yellow' 
+            borderColor='orange'
+            borderWidth='20px'
+            hoverTextColor='white'
+            hoverBgColor='orange'
+            size='large'
+            disabled={false}
+            onClick={() => alert("Has hecho click")}
+          />
+        </CardContent>
+      </Card>
 
-        <Typography variant="h6" component="div">
-          {nombre} en {pais}
-        </Typography>
-        <Typography variant="subtitle1" color="text.secondary">
-          {cargo} en {empresa}
-        </Typography>
-        <Typography variant="body2" color="text.secondary" sx={{ marginTop: '8px' }}>
-          "{testimonio}"
-        </Typography>
-        
-        <IconButton onClick={handlerMeGusta}>
-          {/* color="error" es rojo */}
-          {gustado ? <FavoriteIcon color="error" /> : <FavoriteBorderIcon />}
-        </IconButton>
-
-      </CardContent>
-    </Card>
+      <MoisesAntonioPC
+        titulo="Título 1"
+        subcabecera="Subcabecera 1"
+        texto="Texto de ejemplo 1"
+        imagen={imagen}
+        textoBoton="Texto botón"
+        onFavorite={() => alert("Has hecho click en Me Gusta")}
+        onShare={() => alert("Has hecho click en Compartir")}
+        onAction={() => alert("Has hecho click en el botón genérico")}
+        desactivarCompartir={false}
+        desactivarMeGusta={false}
+        desactivarBoton={false}
+        colorBotonMeGusta="red"
+        colorBotonCompartir="blue"
+        colorBoton="green"
+      />
+      </div>
+    </>
   );
 };
 
